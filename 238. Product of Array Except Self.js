cde81011bba66d12a9f0b7356b649ef85dfe8b2b
rahiqalmakhtum <rahiq789@gmail.com>
2025-10-28 09:59:54 +0600

    solved 238

---
+++ 238. Product of Array Except Self.js
@@ -0,0 +1,23 @@
+var productExceptSelf = function (nums) {
+  let left = new Array(nums.length);
+  let right = new Array(nums.length);
+  let answer = [];
+
+  left[0] = 1;
+  for (let i = 1; i < nums.length; i++) {
+    left[i] = left[i - 1] * nums[i - 1];
+  }
+
+  right[nums.length - 1] = 1;
+  for (let i = nums.length - 2; i >= 0; i--) {
+    right[i] = right[i + 1] * nums[i + 1];
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    answer[i] = left[i] * right[i];
+  }
+
+  return answer;
+};
+
+console.log(productExceptSelf([1, 2, 3, 4]));
